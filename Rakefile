80ed9bd6cc5eb91bc0374c7689f96c93bbe08f0a
Julik <me@julik.nl>
2011-07-07 11:09:25 +0200

    Byebye test-spec

--- Rakefile
+++ Rakefile
@@ -3,10 +3,10 @@ require 'rubygems'
 require 'hoe'
 require File.dirname(__FILE__) + "/lib/sequencer"
 
+Hoe::RUBY_FLAGS.gsub!(/^-w/, '')
 Hoe.spec 'sequencer' do | s |
   s.version = Sequencer::VERSION
   s.developer('Julik Tarkhanov', 'me@julik.nl')
-  s.extra_dev_deps = {"test-spec" => ">=0"}
 end
 
 # vim: syntax=ruby
--- test/test_sequencer.rb
+++ test/test_sequencer.rb
@@ -1,10 +1,9 @@
 require 'test/unit'
 require 'rubygems'
-require 'test/spec'
 require 'fileutils'
 require File.dirname(__FILE__) + '/../lib/sequencer'
 
-TEST_DIR = File.dirname(__FILE__) + "/tmp"
+TEST_DIR = File.expand_path(File.dirname(__FILE__)) + "/tmp"
 
 def emit_test_dirs
   start_f = 123
@@ -59,150 +58,155 @@ def teardown_test_dirs
   FileUtils.rm_rf(TEST_DIR)
 end
 
-context "Sequencer.glob_and_padding_for should" do
+class Test_glob_and_padding_for < Test::Unit::TestCase
   
-  specify "return proper glob pattern and padding for a path with extension" do
+  def test_returns_proper_glob_pattern_padding_for_a_path_with_extension
     glob, pad = Sequencer.glob_and_padding_for("file.00001.gif")
-    glob.should.equal "file.[0-9][0-9][0-9][0-9][0-9].gif"
-    pad.should.equal 5
+    assert_equal "file.[0-9][0-9][0-9][0-9][0-9].gif", glob
+    assert_equal 5, pad
   end
-
-  specify "return proper glob pattern and padding for a path without extension" do
+  
+  def test_returns_proper_glob_pattern_and_padding_for_a_path_without_extension
     glob, pad = Sequencer.glob_and_padding_for("file.00001")
-    glob.should.equal "file.[0-9][0-9][0-9][0-9][0-9]"
-    pad.should.equal 5
+    assert_equal "file.[0-9][0-9][0-9][0-9][0-9]", glob
+    assert_equal 5, pad
   end
   
-  specify "return nil for a file that is not a sequence" do
+  def return_nil_for_a_file_that_is_not_in_the_sequence
      glob, pad = Sequencer.glob_and_padding_for("file")
-     glob.should.be.nil
+     assert_nil glob
   end
 end
 
-context "Sequencer.entries should" do
-  before { emit_test_dirs }
-  after { teardown_test_dirs }
-  specify "return entries for every sequence in a directory" do
+class Sequencer_entries_should < Test::Unit::TestCase
+  def setup; emit_test_dirs; end
+  def teardown; teardown_test_dirs; end
+  
+  def test_returns_entries_for_every_sequence_in_a_directory
     entries = Sequencer.entries(TEST_DIR + "/many_seqs")
+    flunk "this test is not finished"
   end
 end
 
-context "A Sequence created from unpadded files should" do
-  before { emit_test_dirs }
-  after { teardown_test_dirs }
+class A_Sequence_created_from_unpadded_files_should < Test::Unit::TestCase
+  def setup; emit_test_dirs; end
+  def teardown; teardown_test_dirs; end
   
-  specify "be properly created" do
+  def test_properly_created
     s = Sequencer.from_single_file(TEST_DIR + "/natural_numbering/somefiles 5545168.png")
-    s.should.not.be.nil
-    s.expected_frames.should.equal 30
-    s.file_count.should.equal 30
-    s.pattern.should.equal "somefiles %07d.png"
+    assert_not_nil s
+    assert_equal 30, s.expected_frames
+    assert_equal 30, s.file_count
+    assert_equal "somefiles %07d.png", s.pattern
   end
 end
 
-context "A Sequence created from a file that has no numbering slot should" do
-  before { @single = Sequencer::Sequence.new("/tmp", ["foo.tif"]) }
+class A_Sequence_created_from_a_file_that_has_no_numbering_slot_should
+  def setup; @single = Sequencer::Sequence.new("/tmp", ["foo.tif"]); end
   
-  specify "report a pattern that is the same as filename" do
-    @single.pattern.should.equal "foo.tif"
-    @single.should.be.single_file?
-    @single.expected_frames.should.equal 1
-    @single.inspect.should.equal "#<foo.tif>"
+  def test_report_a_pattern_that_is_the_name_as_filename
+    assert_equal 'foo.tif', @single.pattern
+    assert @single.single_file?
+    assert_equal 1, @single.expected_frames
+    assert_equal "#<foo.tif>", @single.inspect
   end
 end
 
-context "Sequencer#bulk_rename should" do
-  before do
+class Sequencer_bulk_rename_should < Test::Unit::TestCase
+  def setup
     emit_test_dirs
     @with_gaps = Sequencer.from_single_file(TEST_DIR + "/sequence_and_sole_file/broken_seq.000245.tif")
   end
   
-  after { teardown_test_dirs }
+  def teardown
+    teardown_test_dirs
+  end
   
-  specify "return a new Sequencer with right parameters" do
+  def test_return_a_new_Sequencer_with_right_parameters
     res = @with_gaps.bulk_rename("another_base")
-    res.length.should.equal(@with_gaps.length)
-    res.directory.should.equal(@with_gaps.directory)
-    res.pattern.should.equal("another_base.%03d.tif")
+    assert_equal @with_gaps.length, res.length
+    assert_equal @with_gaps.directory, res.directory
+    assert_equal "another_base.%03d.tif", res.pattern
     
     assert File.exist?(res.to_paths[0])
     assert File.exist?(res.to_paths[-1])
   end
 end
 
-context "A Sequence created from pad-numbered files should" do
-  before do
+class A_Sequence_created_from_pad_numbered_files_should < Test::Unit::TestCase
+  def setup
     emit_test_dirs
     @gapless = Sequencer.from_single_file(TEST_DIR + "/sequence_and_sole_file/seq1.000245.tif")
     @with_gaps = Sequencer.from_single_file(TEST_DIR + "/sequence_and_sole_file/broken_seq.000245.tif")
     @single = Sequencer.from_single_file(TEST_DIR + "/sequence_and_sole_file/single_file.002123154.tif")
   end
   
-  after { teardown_test_dirs }
+  def teardown
+    teardown_test_dirs
+  end
   
-  specify "initialize itself from one path to a file in the sequence without gaps" do
-    @gapless.should.not.be.nil
-    @gapless.should.be.kind_of(Sequencer::Sequence)
-    @gapless.should.respond_to(:gaps?)
-    @gapless.should.not.be.single_file?
+  def test_initialize_itself_from_one_path_to_a_file_in_the_sequence_without_gaps
+    assert_not_nil @gapless
+    assert_kind_of Sequencer::Sequence, @gapless
+    assert_respond_to @gapless, :gaps?
+    assert !@gapless.single_file?
     
-    @gapless.should.blaming("this is a gapless sequence").not.be.gaps?
-    @gapless.file_count.should.blaming("actual file count in sequence").equal(446)
-    @gapless.length.should.blaming("actual file count in sequence").equal(446)
-    @gapless.expected_frames.should.blaming("expected frame count in sequence").equal(446)
-    @gapless.inspect.should.blaming("inspect itself").equal('#<seq1.[123..568].tif>')
-    @gapless.pattern.should.equal 'seq1.%06d.tif'
+    assert !@gapless.gaps?, "this is a gapless sequence"
+    assert_equal 446, @gapless.file_count, "actual file count in sequence"
+    assert_equal 446, @gapless.length, "actual file count in sequence"
+    assert_equal 446, @gapless.expected_frames, "expected frame count in sequence"
+    assert_equal '#<seq1.[123..568].tif>', @gapless.inspect
     
     files = @gapless.to_a
-    files.length.should.equal 446
-    files[0].should.equal 'seq1.000123.tif'
+    assert_equal 446, files.length
+    assert_equal 'seq1.000123.tif', files[0] 
     
     paths = @gapless.to_paths
-    paths.length.should.equal 446
-    paths[0].should.equal(File.dirname(__FILE__) + "/tmp/sequence_and_sole_file/seq1.000123.tif")
+    assert_equal 446, paths.length
+    assert_equal File.expand_path(File.dirname(__FILE__)) + "/tmp/sequence_and_sole_file/seq1.000123.tif", paths[0]
   end
   
-  specify "initialize itself from one path to a file in the sequence with gaps" do
-    @with_gaps.should.not.be.nil
-    @with_gaps.should.be.kind_of(Sequencer::Sequence)
-    @with_gaps.should.not.be.single_file?
+  def test_initialize_itself_from_one_path_to_a_file_in_the_sequence_with_gaps
+    assert_not_nil @with_gaps
+    assert_kind_of Sequencer::Sequence, @with_gaps
+    assert !@with_gaps.single_file?
     
-    @with_gaps.should.be.gaps?
-    @with_gaps.gap_count.should.equal 1
-    @with_gaps.segment_count.should.equal 2
-    @with_gaps.missing_frames.should.equal(9)
-    @with_gaps.inspect.should.blaming("inspect itself").equal('#<broken_seq.[123..568, 578..702].tif>')
-    @with_gaps.should.include("broken_seq.000123.tif")
-    @with_gaps.should.not.include("bogus.123.tif")
+    assert @with_gaps.gaps?
+    assert_equal 1, @with_gaps.gap_count
+    assert_equal 2, @with_gaps.segment_count
+    assert_equal 9, @with_gaps.missing_frames
+    assert_equal '#<broken_seq.[123..568, 578..702].tif>', @with_gaps.inspect
+    assert @with_gaps.include?("broken_seq.000123.tif")
+    assert !@with_gaps.include?("bogus.123.tif")
   end
   
-  specify "return subsequences without gaps" do
+  def test_return_subsequences_without_gaps
     subseqs = @with_gaps.to_sequences
-    subseqs[0].should.be.kind_of(Sequencer::Sequence)
-    subseqs[1].should.be.kind_of(Sequencer::Sequence)
+    assert_kind_of Sequencer::Sequence, subseqs[0]
+    assert_kind_of Sequencer::Sequence, subseqs[1]
     
     first_seq, second_seq = subseqs
-    first_seq.first_frame_no.should.equal 123
-    first_seq.last_frame_no.should.equal 568
-    second_seq.first_frame_no.should.equal 578
-    second_seq.last_frame_no.should.equal 702
+    assert_equal 123, first_seq.first_frame_no
+    assert_equal 568, first_seq.last_frame_no
+    assert_equal 578, second_seq.first_frame_no
+    assert_equal 702, second_seq.last_frame_no
     
-    first_seq.directory.should.equal second_seq.directory
-    first_seq.directory.should.equal @with_gaps.directory
+    assert_equal second_seq.directory, first_seq.directory
+    assert_equal @with_gaps.directory, first_seq.directory 
   end
   
-  specify "list all sequences in directory and subdirectories using the pattern" do
+  def test_list_all_sequences_in_directory_and_subdirectories_using_the_pattern
     s = Sequencer.from_glob(TEST_DIR + "/**/*.tif")
     inspected = '[#<single.tif>, #<seq1.[458..512].tif>, #<anotherS [228..312].tif>, #<in_subdir [445..471].tif>, #<seq1.[123..568].tif>, #<somefile.tif>, #<single_file.002123154.tif>, #<broken_seq.[123..568, 578..702].tif>]'
-    s.inspect.should.equal inspected
+    assert_equal inspected, s.inspect
   end
   
-  specify "initialize itself from a single file" do
-    @single.should.be.single_file?
-    @single.inspect.should.equal '#<single_file.002123154.tif>'
-    @single.should.not.be.gaps?
-    @single.expected_frames.should.equal 1
-    @single.file_count.should.equal 1
+  def test_initialize_itself_from_a_single_file
+    assert @single.single_file?
+    assert_equal '#<single_file.002123154.tif>', @single.inspect 
+    assert !@single.gaps?
+    assert_equal 1, @single.expected_frames
+    assert_equal 1, @single.file_count
   end
   
 end
